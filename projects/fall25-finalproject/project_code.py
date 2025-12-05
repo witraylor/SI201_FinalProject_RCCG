611d7c4d2ae8905a70ab5235e3988ecc37a667f3
claireaf-crypto <claireaf@umich.edu>
2025-12-05 16:32:52 -0500

    imported more stuff

--- projects/fall25-finalproject/project_code.py
+++ projects/fall25-finalproject/project_code.py
@@ -1,6 +1,12 @@
 import json
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import csv
+import matplotlib.pyplot as plt
+import os
+import sqlite3
+from typing import List, Dict, Any, Optional
+from datetime import datetime
 
 #hello does this work
 

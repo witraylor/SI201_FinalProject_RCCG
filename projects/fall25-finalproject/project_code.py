b77f570b2fa4995e59945fcfceeea118a28c1202
claireaf-crypto <claireaf@umich.edu>
2025-12-06 14:04:29 -0500

    import spotipy API and started SQlite databases for all 3 APIs

--- projects/fall25-finalproject/project_code.py
+++ projects/fall25-finalproject/project_code.py
@@ -8,6 +8,77 @@ import sqlite3
 from typing import List, Dict, Any, Optional
 from datetime import datetime
 
-#hello does this work
 
-#hello this is anna
+#spotipy ids
+CLIENT_ID = 'be1ea16df5c24ab195ff21e6c8a82cd1'
+CLIENT_SECRET = 'fe32b6a7ae92441bb57db162f56e75a3'
+#create an authorized variable to use for future API calls
+sp = spotipy.Spotify(
+    auth_manager=SpotifyClientCredentials(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET
+    )
+)
+
+def get_spotify_data(query="year:2023", limit=25, offset=0):
+    # Search for tracks matching query
+    results = sp.search(q=query, type="track", limit=limit, offset=offset)
+    tracks = []
+    for item in results.get('tracks', {}).get('items', []):
+        track = {
+        "id": item.get("id"),  # Spotify track ID (string)
+        "name": item.get("name"),
+        "album": item.get("album", {}).get("name"),
+        "artist": item.get("artists", [{}])[0].get("name") if item.get("artists") else None,
+        "artist_id": item.get("artists", [{}])[0].get("id") if item.get("artists") else None,
+        "popularity": item.get("popularity"),
+        "release_date": item.get("album", {}).get("release_date")
+        }
+        tracks.append(track)
+    return tracks
+
+def init_database(db_name="media_data.db"):
+
+    conn = sqlite3.connect(db_name)
+    cur = conn.cursor()
+    # Enable foreign key constraints (in case we use them)
+    cur.execute("PRAGMA foreign_keys = ON;")
+    # Create Movies table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Movies (
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            release_date TEXT,
+            popularity REAL,
+            revenue INTEGER,
+            avg_rating REAL,
+            genre_ids TEXT
+        )
+    """)        
+    # Create Songs table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Songs (
+            id TEXT PRIMARY KEY,
+            title TEXT,
+            artist TEXT,
+            album TEXT,
+            popularity INTEGER,
+            release_date TEXT,
+            genre TEXT
+
+        )
+    """)
+    # Create Shows table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Shows (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            premiere_date TEXT,
+            avg_rating REAL,
+            genres TEXT,
+            weight INTEGER
+        );
+    """)
+    conn.commit()
+    return conn
+
